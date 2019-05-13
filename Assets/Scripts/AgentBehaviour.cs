914c1518420067109aa0407637981b7e482cf416
JMicahMan <j.micah.manzanares@gmail.com>
2019-05-13 18:22:32 -0500

    comit

---
+++ Assets/Scripts/AgentBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentBehaviour : MonoBehaviour {
+
+
+    public GameObject GObj;
+
+    public Transform GTran;
+
+    public Vector3 Velocity;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        Velocity = Vector3.up;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.GetComponent<AgentBehaviour>();
+	}
+}
